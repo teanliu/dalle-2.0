c96189bc0226b668b3df021955d67738a10a90e6
teanliu <teanliuu@gmail.com>
2023-08-04 04:49:37 +0800

    fix: typos

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
-const port = 8080
+import connectDB from './mongodb/connect.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+import postRoutes from './routes/postRoutes.js';
+
+const port = 8080;
 
 dotenv.config();
 
@@ -10,14 +14,22 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }))
 
+app.use('/api/v1/post', postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
 app.get('/', async (req, res) => {
   res.send("Hello from the other side");
 })
 
 const startServer = async () => {
-  app.listen(port, () => {
-    console.log(`server has started on port http://localhost:${port}`)
-  })
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(port, () => {
+      console.log(`server has started on port http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log(error)
+  }
 };
 
 startServer();
